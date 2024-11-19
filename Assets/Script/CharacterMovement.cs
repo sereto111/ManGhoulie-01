696742c9d14b131194cd08b19d793398882fe414
emriesvc <emr.ies.vc@gmail.com>
2024-11-19 13:35:53 +0100

    v.0.0.2 programación de movimietno horizontal

---
+++ Assets/Script/CharacterMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+	public float maxSpeed = 6.0f;
+	public bool facingRight = true;
+	public float moveDirection;
+	private Rigidbody rigidbody;
+	
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent <Rigidbody> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveDirection = Input.GetAxis ("Horizontal");
+
+	}
+	void FixedUpdate () {
+		rigidbody.velocity = new Vector2 (moveDirection * maxSpeed, rigidbody.velocity.y);
+	}
+}
