60d5230b4c9dee6193f0244d7bba9fec18e23da7
sereto111 <sereto562@gmail.com>
2024-11-26 13:43:29 +0100

    cap2 funcional

--- Assets/Script/CharacterMovement.cs
+++ Assets/Script/CharacterMovement.cs
@@ -1,22 +1,14 @@
-<<<<<<< HEAD
+
 using System.Collections;
-=======
-﻿using System.Collections;
->>>>>>> origin/programador1
+
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour {
 
-<<<<<<< HEAD
-	public bool facingRight = true;
-	public float moveDirection;
-	
-	
 	// Use this for initialization
-	void Start () {
 		
-=======
+
 	public float maxSpeed = 6.0f;
 	public bool facingRight = true;
 	public float moveDirection;
@@ -25,20 +17,18 @@ public class CharacterMovement : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		rigidbody = GetComponent <Rigidbody> ();
->>>>>>> origin/programador1
 	
 	}
 	
 	// Update is called once per frame
-<<<<<<< HEAD
+
     
 	void Update () {
-		moveDirection = Input.GetAxis ("Horizontal");
-
-		
+		moveDirection = Input.GetAxis ("Horizontal");		
 	}
 
 	void FixedUpdate () {
+		rigidbody.velocity = new Vector2 (moveDirection * maxSpeed, rigidbody.velocity.y);
 		if (moveDirection > 0.0f && !facingRight) {
 			Flip ();
 		} else if (moveDirection < 0.0f && facingRight) {
@@ -52,14 +42,4 @@ public class CharacterMovement : MonoBehaviour {
 		transform.Rotate (Vector3.up, 180.0f, Space.World);
 	}
 
-
-=======
-	void Update () {
-		moveDirection = Input.GetAxis ("Horizontal");
-
-	}
-	void FixedUpdate () {
-		rigidbody.velocity = new Vector2 (moveDirection * maxSpeed, rigidbody.velocity.y);
-	}
->>>>>>> origin/programador1
 }
