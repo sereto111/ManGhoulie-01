e55ecb455c913cc1ce09f79f40b57688cd679b58
sereto111 <sereto562@gmail.com>
2024-12-03 13:45:22 +0100

    groundcheck0.1

--- Assets/Script/CharacterMovement.cs
+++ Assets/Script/CharacterMovement.cs
@@ -13,22 +13,35 @@ public class CharacterMovement : MonoBehaviour {
 	public bool facingRight = true;
 	public float moveDirection;
 	private Rigidbody rigidbody;
+
+	public float jumpSpeed = 600.0f;
+
+	public bool grounded = false;
+	public Transform groundCheck;
+	public float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
 	
 	// Use this for initialization
 	void Start () {
 		rigidbody = GetComponent <Rigidbody> ();
-	
+		groundCheck = GameObject.Find("GroundCheck").transform;
 	}
 	
 	// Update is called once per frame
 
     
 	void Update () {
-		moveDirection = Input.GetAxis ("Horizontal");		
+		moveDirection = Input.GetAxis ("Horizontal");
+
+		if(grounded && Input.GetButtonDown("Jump"))
+		{
+			rigidbody.AddForce(new Vector2(0, jumpSpeed));
+		}
 	}
 
 	void FixedUpdate () {
 		rigidbody.velocity = new Vector2 (moveDirection * maxSpeed, rigidbody.velocity.y);
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
 		if (moveDirection > 0.0f && !facingRight) {
 			Flip ();
 		} else if (moveDirection < 0.0f && facingRight) {
